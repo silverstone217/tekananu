8a896f947892dbb220d2447dfe14e541bafc77ec
Stephane Mfuni <stephuni35@gmail.com>
2025-08-20 15:55:20 +0100

    Mise à jour de la mise en page de la page d'accueil et suppression d'une cible binaire dans la configuration Prisma

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,18 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="pt-16">
+    <div className="pt-16 lg:pt-18">
       <Header />
-      <h1>Bienvenue à Teka Nanu</h1>
-      <p>
-        Votre solution tout-en-un pour acheter et vendre des produits de seconde
-        main.
-      </p>
-      <Link href="/path/to/your/page">
-        <Button>Cliquez ici pour en savoir plus</Button>
-      </Link>
+      <div className="max-w-7xl mx-auto p-4 space-y-2 w-full ">
+        <h1>Bienvenue à Teka Nanu</h1>
+        <p>
+          Votre solution tout-en-un pour acheter et vendre des produits de
+          seconde main.
+        </p>
+        <Link href="/path/to/your/page">
+          <Button>Cliquez ici pour en savoir plus</Button>
+        </Link>
+      </div>
     </div>
   );
 }
